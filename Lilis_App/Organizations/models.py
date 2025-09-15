3793ef053f3eeb3e3aa6e97ab9a0194fa3bcb1b2
Francisco Astudillo <fco.exe@gmail.com>
2025-09-15 10:43:19 -0300

    Se Agregó el modelo Productos

--- Lilis_App/Organizations/models.py
+++ Lilis_App/Organizations/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    STATES = [
+        ("ACTIVE", "Active"),
+        ("INACTIVE", "Inactive"),
+    ]
+
+    state = models.CharField(max_length=10, choices=STATES, default="ACTIVE")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Organization(BaseModel):
+    name = models.CharField(max_length=120, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Products(BaseModel):
+    nombre  = models.CharField(max_length=200,unique=True)
+    cantidad = models.IntegerField()
+    descripcion = models.CharField(max_length=300)
+    tipoProducto = models.CharField(max_length=100)
+    stockMinimo = models.FloatField()
+    stockMaximo = models.FloatField()
